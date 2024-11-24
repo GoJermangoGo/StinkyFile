7c4e6677babcec5acf8c807f7456744d468ab146
luisfonsivevo <fonsi@brownbricks.com>
2024-11-24 14:27:10 +0200

    mspaint logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "@components/util/Link";
 export function Header() {
 	return (
 		<>
-			<h1 className="pb-4 text-7xl font-bold">Stinky File</h1>
+			<img src="/logo.png"></img>
 			<span className="italic">
 				Free, <Link url="https://github.com/GoJermangoGo/StinkyFile">open source</Link>, cross-platform file
 				transfer service. Gee whiz; those files sure do stink.
